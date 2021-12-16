fc0dc3cf6bef015df4faafa28b8e6e6856e5743f
Victor Werngren <victorlillowerngren@gmail.com>
2021-12-16 21:41:39 +0100

    "tutorial"

--- src/components/TitleScreen.jsx
+++ src/components/TitleScreen.jsx
@@ -6,6 +6,20 @@ const TitleScreen = ({ startGame }) => {
       <div>
         <h1>Reactoids</h1>
         <p>- Click to start -</p>
+        <div className='tutorial'>
+          <div className='tutorialKeys'>
+            <div></div>
+            <div className='tutorialKey'>W</div>
+            <div></div>
+            <div className='tutorialKey'>A</div>
+            <div className='tutorialKey'>S</div>
+            <div className='tutorialKey'>D</div>
+          </div>
+          <div className='tutorialShoot'>
+            <p>Shoot:</p>
+            <span className='tutorialSpaceKey'>Space</span>
+          </div>
+        </div>
       </div>
     </div>
   )
