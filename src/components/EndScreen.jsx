808b3211540af6f7a1d0192e00c0626303158164
Victor Werngren <victorlillowerngren@gmail.com>
2021-12-20 20:41:32 +0100

    still working on a cool input field

--- src/components/EndScreen.jsx
+++ src/components/EndScreen.jsx
@@ -1,11 +1,31 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const EndScreen = ({ startGame, highscore }) => {
-  const [state, setState] = useState(0)
+  const [state, setState] = useState(2)
+  const [nameInput, setNameInput] = useState('TEST')
+  const inputRefs = useRef([])
+
+  const handleNameInput = (e, i) => {
+    console.log("nameInput[i]", nameInput[i])
+    setNameInput(prev => (prev.substring(0, i) + e.target.value).toUpperCase() /* + prev.substring(i +1) */)
+  }
+
+  const handleClick = () => {
+    console.log(inputRefs.current)
+    inputRefs.current[nameInput.length].focus()
+  }
 
   const fillDots = (item) => {
     return '.'.repeat(35 - (item.name.length + String(item.score).length))
   }
+
+  const fillRefs = (el) => {
+    if (el && !inputRefs.current.includes(el)) inputRefs.current.push(el)
+  }
+
+  useEffect(() => {
+    inputRefs.current[nameInput.length].focus()
+  })
 
   return (
     <>
@@ -20,14 +40,15 @@ const EndScreen = ({ startGame, highscore }) => {
           <p className='highscoreText'>Highscore</p>
           {highscore &&
             <div className='highscoreList'>
-              {highscore.map((item, i) => (<p className='highscoreItem' style={{ '--delay': i }} key={item.id}>{item.name}{fillDots(item)}{item.score}</p>))}
+              {highscore.map((item, i) => (<p className='highscoreItem' style={{ '--delay': `${i / 10}s` }} key={item.id}>{item.name}<span>{fillDots(item)}</span>{item.score}</p>))}
             </div>
           }
         </div>
       }
       {state === 2 &&
-        <div className='nameInputWrapper'>
-
+        <div className='nameInputWrapper' onClick={() => handleClick()}>
+          <p>New Highscore!</p>
+          {[...Array(6)].map((item, i) => (<input type="text" key={i} className={``} value={nameInput[i] ?? ''} onChange={(e) => handleNameInput(e, i)} ref={fillRefs} />))}
         </div>
       }
     </>
